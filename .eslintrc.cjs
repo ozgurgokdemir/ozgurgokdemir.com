7907731cccf54a5b295f1992f39b15695f2ce29a
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-06-02 20:02:59 +0300

    fix: Linting / Parsing Erors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,54 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true,
-    es2022: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:astro/recommended',
-    'prettier',
-  ],
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-    ecmaVersion: 'latest',
-  },
   overrides: [
     {
       files: ['*.astro'],
+      env: {
+        browser: true,
+        node: true,
+        es2022: true,
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:astro/recommended',
+        'prettier',
+      ],
+      plugins: ['@typescript-eslint', 'astro'],
       parser: 'astro-eslint-parser',
       parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+        ecmaVersion: 'latest',
         parser: '@typescript-eslint/parser',
         extraFileExtensions: ['.astro'],
       },
     },
+    {
+      files: ['*.ts', '*.tsx'],
+      env: {
+        browser: true,
+        node: true,
+        es2022: true,
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+      ],
+      plugins: ['@typescript-eslint'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+      },
+    },
   ],
-  ignorePatterns: ['*.config.cjs', '.eslintrc.cjs'],
+  ignorePatterns: ['*.config.mjs'],
 };
