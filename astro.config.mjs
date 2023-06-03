903983735730cab6958e32353bbba9538fe7d256
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-06-03 19:44:23 +0300

    feat: Integrate Astro Icon + Astrojs Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
 
 export default defineConfig({
-  integrations: [tailwind({ config: { applyBaseStyles: false } })],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
 });
