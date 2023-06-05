3d0a85046e4a591c548ab58bf565a12cc67872e6
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-06-05 16:43:04 +0300

    feat: Add Button Mouse Effect

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,8 @@ module.exports = {
   root: true,
   overrides: [
     {
-      files: ['*.astro'],
+      files: ['**/*.astro'],
       env: {
-        browser: true,
         node: true,
         es2022: true,
       },
@@ -25,7 +24,28 @@ module.exports = {
       },
     },
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['**/*.astro/*.js'],
+      env: {
+        browser: true,
+        es2022: true,
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+      ],
+      plugins: ['@typescript-eslint'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+      },
+    },
+    {
+      files: ['**/*.ts', '**/*.tsx'],
       env: {
         browser: true,
         node: true,
