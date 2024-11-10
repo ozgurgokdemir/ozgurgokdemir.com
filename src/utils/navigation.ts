c3f2688cf8c772eb4ba1e497aab9cff1c29c0dfb
Özgür Gökdemir <ozgurgokd@gmail.com>
2024-11-10 04:54:15 +0300

    feat: improve navigation util

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,4 +1,45 @@
-export function isCurrentPath(pathname: string) {
+import { navigate } from 'astro:transitions/client';
+import sessionStorage from '@/utils/session-storage';
+
+type Navigation = {
+  current: string;
+  previous?: string;
+};
+
+function getHistory() {
+  return sessionStorage.getItem<Navigation>('navigation-history');
+}
+
+function syncHistory() {
+  const navigationHistory = getHistory();
+
+  const updatedNavigationHistory = {
+    current: location.href,
+    previous:
+      navigationHistory?.current !== location.href
+        ? navigationHistory?.current
+        : navigationHistory?.previous,
+  };
+
+  sessionStorage.setItem('navigation-history', updatedNavigationHistory);
+}
+
+function canGoBack() {
+  const navigationHistory = getHistory();
+  return !!navigationHistory?.previous && history.state.index > 0;
+}
+
+function isCurrentPath(pathname: string) {
   const normalize = (path: string) => path.replace(/\/+$/, '');
   return normalize(pathname) === normalize(location.pathname);
 }
+
+export default {
+  back: () => history.back(),
+  forward: () => history.forward(),
+  navigate,
+  getHistory,
+  syncHistory,
+  canGoBack,
+  isCurrentPath,
+};
--- src/utils/session-storage.ts
+++ src/utils/session-storage.ts
@@ -1,8 +1,10 @@
-export function getItem<T>(key: string) {
+function getItem<T>(key: string) {
   const value = sessionStorage.getItem(key);
   return value ? (JSON.parse(value) as T) : null;
 }
 
-export function setItem(key: string, value: unknown) {
+function setItem(key: string, value: unknown) {
   sessionStorage.setItem(key, JSON.stringify(value));
 }
+
+export default { getItem, setItem };
